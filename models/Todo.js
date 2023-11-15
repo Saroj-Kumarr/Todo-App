240e037cd1f3c4aa585ce3f4bac87c351e7c29e1
saroj-mali <sarojmali6090@gmail.com>
2023-11-16 03:36:31 +0530

    Nothing new

--- models/Todo.js
+++ models/Todo.js
@@ -25,6 +25,115 @@ const todoSchema = new mongoose.Schema({
 
 module.exports = mongoose.model("Todo", todoSchema);
 
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
+
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
+
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
+
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
+
+
 
 // const mongoose = require("mongoose");
 
